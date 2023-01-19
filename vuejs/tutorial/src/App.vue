0e757393ed359a2fea99e50dadab4ba98dcd918b
leeseunghee00 <lsh9246@gmail.com>
2023-01-20 03:13:55 +0900

    upload up to Computed Property

--- vuejs/tutorial/src/App.vue
+++ vuejs/tutorial/src/App.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>안녕 Vue!</h1>
+  <div>
+    <DeclarativeRendering />
+    <AttributeBindings />
+    <EventListeners />
+    <FormBindings />
+    <ConditionalRenderin />
+    <ListRendering />
+    <ComputedProperty />
+  </div>
 </template>
+
+<script>
+import DeclarativeRendering from "@/components/DeclarativeRendering.vue";
+import AttributeBindings from "@/components/AttributeBindings.vue";
+import EventListeners from "@/components/EventListeners.vue";
+import FormBindings from "@/components/FormBindings.vue";
+import ConditionalRenderin from "@/components/ConditionalRenderin.vue";
+import ListRendering from "@/components/ListRendering.vue";
+import ComputedProperty from "@/components/ComputedProperty.vue";
+
+
+export default {
+  name: 'App',
+  components: {
+    ComputedProperty,
+    ListRendering,
+    ConditionalRenderin,
+    FormBindings,
+    EventListeners,
+    AttributeBindings,
+    DeclarativeRendering
+  },
+  data() {
+    return {};
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vuejs/tutorial/src/conponents/FormBindings.vue
@@ -0,0 +1,10 @@
+<template>
+  <input v-model="text" placeholder="여기에 입력하기">
+  <p>{{ text }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const text = ref('');
+</script>
