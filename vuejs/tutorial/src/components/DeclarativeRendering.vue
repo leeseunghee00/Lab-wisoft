5437beec9bccdaf57b859447aa9f1d69bcfe2545
leeseunghee00 <lsh9246@gmail.com>
2023-01-20 00:36:23 +0900

    upload up to Computed Property

---
+++ vuejs/tutorial/src/components/DeclarativeRendering.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>{{ message }}</h1>
+  <p>숫자 세기: {{ counter.count }}</p>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+
+const counter = reactive({ count: 0 });
+const message = ref('안녕 Vue!');
+</script>
