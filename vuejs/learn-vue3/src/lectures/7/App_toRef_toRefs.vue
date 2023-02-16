fb722653ac76fdb3a7e735010f4d0da11e1fdb03
leeseunghee00 <lsh9246@gmail.com>
2023-02-16 18:06:12 +0900

    upload up to Props

---
+++ vuejs/learn-vue3/src/lectures/7/App_toRef_toRefs.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>author: {{ author }}</p>
+    <p>title: {{ title }}</p>
+  </div>
+</template>
+
+<script>
+import {reactive, toRef} from "vue";
+
+export default {
+  setup() {
+    const book = reactive({
+      author: "Vue Team",
+      year: "2023",
+      title: "Vue 3 Guide",
+      description: "당신은 지금 바로 이 책을 읽습니다",
+      price: "무료",
+    });
+
+    // 방법1
+    // const {author, title} = toRefs(book);
+
+    // 방법2
+    const author = toRef(book, "author");
+    const title = toRef(book, "title");
+
+    return {
+      author,
+      title,
+      book,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
