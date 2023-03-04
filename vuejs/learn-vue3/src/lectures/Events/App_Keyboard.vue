978798f00e39a9855986a397e44cd20308b60e11
leeseunghee00 <lsh9246@gmail.com>
2023-03-04 20:12:10 +0900

    upload Events

---
+++ vuejs/learn-vue3/src/lectures/Events/App_Keyboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" @keyup.ctrl.enter="addTodo"/>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+
+export default {
+  setup() {
+    const todos = reactive([]);
+
+    const addTodo = e => {
+      console.log("event.key", e.key);
+      e.target.value = "";
+      e.target.focus();
+    };
+    return {todos, addTodo};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
