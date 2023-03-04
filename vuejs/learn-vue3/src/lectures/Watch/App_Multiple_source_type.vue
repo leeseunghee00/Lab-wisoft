4cbadf55515c0c9f76729884e69e4ca8cc33a7c7
leeseunghee00 <lsh9246@gmail.com>
2023-03-04 20:15:43 +0900

    Upload Watch

---
+++ vuejs/learn-vue3/src/lectures/Watch/App_Multiple_source_type.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import {reactive, ref, watch} from "vue";
+
+export default {
+  setup() {
+    const x = ref(0);
+    const y = ref(0);
+
+    const obj = reactive({
+      count: 0,
+    });
+
+    // watch([x,y], ([newX, newY]) => {
+    //   console.log(newX, newY);
+    // });
+
+    // console.log(typeof obj.count);
+
+    // watch(
+    //     () => obj.count,
+    //     (newValue, oldValue) => {
+    //       console.log('newValue: ', newValue);
+    //       console.log('oldValue: ', oldValue);
+    //     });
+
+    // watch(obj,
+    //     (newValue, oldValue) => {
+    //       console.log("newValue: ", newValue);
+    //       console.log("oldValue: ", oldValue);
+    //     });
+
+    const person = reactive({
+      name: "gildong",
+      age: 30,
+      hobby: "health",
+      obj: {
+        count: 0,
+      },
+    });
+
+    // watch(person,
+    //     (newValue) => {
+    //       console.log("newValue: ", newValue);
+    // });
+
+    return {x, y, obj, person};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vuejs/learn-vue3/src/lectures/Watch/App_WatchEffect.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form action="" @submit.prevent>
+      <input v-model.lazy="title" type="text" placeholder="Title"/>
+      <textarea v-model.lazy="contents" placeholder="Contents"></textarea>
+      <hr/>
+      <button>save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import {ref, watchEffect} from "vue";
+
+export default {
+  setup() {
+    const title = ref("");
+    const contents = ref("");
+
+    const save = (title, contents) => {
+      console.log(`저장되었습니다. title: ${title}, contents: ${contents}`);
+    };
+
+    watchEffect(() => {
+      console.log("watchEffect");
+      save(title.value, contents.value);
+    });
+
+    return {title, contents};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
